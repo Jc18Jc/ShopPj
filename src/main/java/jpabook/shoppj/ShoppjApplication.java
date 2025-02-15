571389ce7082e9c725f81529b9c8ba0e9d38e1f6
Jc18Jc <jc5467@naver.com>
2025-02-15 17:27:08 +0900

    init: initial commit, create project

---
+++ src/main/java/jpabook/shoppj/ShoppjApplication.java
@@ -0,0 +1,13 @@
+package jpabook.shoppj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppjApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppjApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/jpabook/shoppj/ShoppjApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.shoppj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
